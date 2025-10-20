fbddb3f1747a011044a7d574edc4d195c234d823
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2025-10-20 11:59:48 +0100

    Set up window in Simulation
    -Added window into the simulation
    -window can be closed with Escape key
    -might attempt to put window into its own class

--- Dissertation project/main.cpp
+++ Dissertation project/main.cpp
@@ -1,5 +1,19 @@
 ﻿
+#include "src/Simulation.h"
+
 int main()
 {
+	Simulation simulation;
+
+	if (!simulation.init())
+	{
+		std::cerr << "Failed to initialise simulation";
+		return 1;
+	}
+
+	simulation.run();
+
+	simulation.~Simulation();
+
 	return 0;
 }
